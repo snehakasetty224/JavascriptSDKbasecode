b1aaa77fdea46287fdae75841662de9e584e7cf3
snehakasetty224 <snehakasettysudarshan@gmail.com>
2017-04-21 19:07:11 -0700

    Add files via upload

---
+++ SnehaWikitudeJavaSetupObjectiveC/SnehaWikitudeJavaSetupObjectiveC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SnehaWikitudeJavaSetupObjectiveC
+//
+//  Created by Sneha Kasetty Sudarshan on 4/20/17.
+//  Copyright © 2017 Sneha Kasetty Sudarshan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SnehaWikitudeJavaSetupObjectiveC/SnehaWikitudeJavaSetupObjectiveC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SnehaWikitudeJavaSetupObjectiveC
+//
+//  Created by Sneha Kasetty Sudarshan on 4/20/17.
+//  Copyright © 2017 Sneha Kasetty Sudarshan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
